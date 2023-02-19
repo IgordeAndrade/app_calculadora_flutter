b28af09f1307724a2bfec7ab14bc6ea15d32fe5b
Igor Andrade <igordeandrade3@gmail.com>
2023-02-19 13:38:14 -0300

    Primeiro commit

--- lib/create_button.dart
+++ lib/create_button.dart
@@ -1,25 +1,33 @@
 import 'package:flutter/material.dart';
 
-Expanded createButton({
-  required String caracter,
-  required void Function() press,
-  required Color color,
-  required BuildContext context,
-}) {
-  return Expanded(
-    child: SizedBox(
-      height: MediaQuery.of(context).size.height * 0.13,
-      child: ElevatedButton(
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.black12,
-          padding: const EdgeInsets.all(20),
-        ),
-        onPressed: press,
-        child: Text(
-          caracter,
-          style: TextStyle(color: color, fontSize: 30),
+class DigitButton extends StatelessWidget {
+  const DigitButton({
+    super.key,
+    required this.caracter,
+    required this.onPressed,
+    required this.color,
+  });
+  final String caracter;
+  final void Function() onPressed;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height * 0.13,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.black12,
+            padding: const EdgeInsets.all(20),
+          ),
+          onPressed: onPressed,
+          child: Text(
+            caracter,
+            style: TextStyle(color: color, fontSize: 30),
+          ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
